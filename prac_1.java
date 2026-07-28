de7c59682fad741c72473d88cbfc2953df4ec6b9
Bhupendra S <25ds3000042@ds.study.iitm.ac.in>
2026-07-28 23:16:33 +0530

    Add files via upload

---
+++ prac_1.java
@@ -0,0 +1,16 @@
+
+
+
+public class prac_1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+
+        System.out.println("Welcome");
+
+        System.out.println("Java");
+
+    }
+
+}
