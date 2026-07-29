b708e70f886bd9e5410078e858da5f28fd690cf2
Bhupendra S <25ds3000042@ds.study.iitm.ac.in>
2026-07-29 06:09:41 +0530

    Add files via upload

---
+++ main_add.java
@@ -0,0 +1,14 @@
+
+
+public class main_add {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+
+        System.out.println(sum);
+
+    }
+}
