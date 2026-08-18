c787c57caf1e7c6a85b177e200d49718a1188112
Bhupendra S <25ds3000042@ds.study.iitm.ac.in>
2026-08-18 23:54:05 +0530

    Add files via upload

---
+++ if_statement.java
@@ -0,0 +1,12 @@
+public class if_statement {
+    public static void main(String[] args) {
+
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Minor");
+        }
+    }
+}
