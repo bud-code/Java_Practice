462661b61646dd8ca66a3a307e8b6937e420357e
Bhupendra S <25ds3000042@ds.study.iitm.ac.in>
2026-08-18 23:58:45 +0530

    Add files via upload

---
+++ largest_of_three.java
@@ -0,0 +1,18 @@
+public class largest_of_three {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 25;
+        int c = 15;
+
+        if (a > b && a > c) {
+            System.out.println("A is largest");
+        }
+        else if (b > a && b > c) {
+            System.out.println("B is largest");
+        }
+        else {
+            System.out.println("C is largest");
+        }
+    }
+}
