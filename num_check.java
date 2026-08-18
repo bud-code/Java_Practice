18d0acf222d4fb72d35e6ea5fcd1edc7f3bb5948
Bhupendra S <25ds3000042@ds.study.iitm.ac.in>
2026-08-18 23:57:36 +0530

    Add files via upload

---
+++ num_check.java
@@ -0,0 +1,16 @@
+public class num_check {
+    public static void main(String[] args) {
+
+        int number = -10;
+
+        if (number > 0) {
+            System.out.println("Positive");
+        } 
+        else if (number < 0) {
+            System.out.println("Negative");
+        } 
+        else {
+            System.out.println("Zero");
+        }
+    }
+}
